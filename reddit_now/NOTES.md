a4513097a3ceb3d872f2e188f4768a5b184979ee
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-09 19:43:47 +0000

    first commit

---
+++ reddit_now/NOTES.md
@@ -0,0 +1,6 @@
+What is a post
+a post has a title
+post has a user
+post has url
+post has upvotes
+
---
+++ reddit_now/lib/reddit_now.rb
@@ -0,0 +1,3 @@
+require_relative "reddit_now/version"
+require_relative './reddit_now/cli'
+require_relative './reddit_now/posts'
